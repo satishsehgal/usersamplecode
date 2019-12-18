a87ab8f99e48bf6467e3d3c17e2a76a840d360a4
satish6677 <satish.k6677@gmail.com>
2019-12-19 02:04:44 +0530

    added comment to api

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -7,10 +7,12 @@ class V1::UsersController < V1::ApplicationController
   expose(:users) { User.all_users }
   expose(:user, scope: :without_deleted)
 
+  #Get list of users
   def index
     respond_with users, root: :users, each_serializer: V1::UserSerializer
   end
 
+  #Show user by id
   def show
   	respond_with user, root: :user, serializer: V1::UserSerializer
   end
