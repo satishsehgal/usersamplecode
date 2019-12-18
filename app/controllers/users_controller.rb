94f3a8728ba678dac1399b239b42623302121bf6
satish6677 <satish.k6677@gmail.com>
2019-12-19 02:10:40 +0530

    updated alert message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  CREATE_SUCCESS = "User was successfully created."
+  UPDATE_SUCCESS = "User was successfully updated."
+  DESTROY_SUCCESS = "User was successfully destroyed."
 
   expose(:users) { User.all_users }
   expose(:user, scope: :without_deleted)
@@ -25,7 +28,7 @@ class UsersController < ApplicationController
   def create
     new_user = User.new(user_params)
     if new_user.save
-      redirect_to new_user, notice: 'User was successfully created.'
+      redirect_to new_user, notice: CREATE_SUCCESS
     else
       render :new
     end
@@ -34,7 +37,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if user.update(user_params)
-      redirect_to user, notice: 'User was successfully updated.'
+      redirect_to user, notice: UPDATE_SUCCESS
     else
       render :edit
     end
@@ -43,7 +46,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    redirect_to users_url, notice: DESTROY_SUCCESS
   end
 
   private
