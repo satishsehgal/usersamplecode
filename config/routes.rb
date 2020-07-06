7e3f136b9d7041d35c347882cf44761a245bf0fd
satishsehgal <satish.k6677@gmail.com>
2020-07-06 12:45:48 +0530

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :users, only: %i(index show)
   end
+
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
