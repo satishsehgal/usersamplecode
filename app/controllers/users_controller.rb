467541208f23ac1ebac7d2121729e913c4358431
satishsehgal <satish.k6677@gmail.com>
2020-07-06 14:08:16 +0530

    added last name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :last_name)
     end
 end
